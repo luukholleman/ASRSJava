b0ba90675ac8385da9e0c351650cfab54efb87e5
Luuk Holleman <luukholleman@gmail.com>
2013-04-16 14:05:35 +0200

    xml panel is done

--- src/XMLPanel.java
+++ src/XMLPanel.java
@@ -9,18 +9,22 @@ import javax.swing.JPanel;
 
 public class XMLPanel extends JPanel {
 	private JButton xmlBtn = new JButton("XML Inlezen");
+	private JLabel fileLbl = new JLabel("Geen bestand gekozen");
 	
 	public XMLPanel()
 	{
 		setBorder(BorderFactory.createTitledBorder("XML Inlezen"));
 		
-		setPreferredSize(new Dimension(500, 200));
+		setPreferredSize(new Dimension(500, 160));
 		
 		buildUI();
 	}
 	
 	private void buildUI()
 	{
+		// maak er een big ass button van
+		xmlBtn.setPreferredSize(new Dimension(480, 100));
 		add(xmlBtn);
+		add(fileLbl);
 	}
 }
