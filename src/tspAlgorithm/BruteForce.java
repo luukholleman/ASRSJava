8b4165a6e39d024ab88960797e79f821a917989b
Luuk Holleman <luukholleman@gmail.com>
2013-04-16 14:55:56 +0200

    added tsp algorithms

---
+++ src/tspAlgorithm/BruteForce.java
@@ -0,0 +1,14 @@
+/**
+ * @author Luuk Holleman
+ * @date 15 april
+ */
+package tspAlgorithm;
+
+public class BruteForce implements TSPAlgorithm {
+
+	@Override
+	public String getName() {
+		return "Brute Force";
+	}
+
+}
