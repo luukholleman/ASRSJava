5f2ef051f8c30cf87c0d16a71a5c04c87126a165
Luuk Holleman <luukholleman@gmail.com>
2013-04-16 14:55:56 +0200

    added tsp algorithms

---
+++ src/tspAlgorithm/Greedy.java
@@ -0,0 +1,14 @@
+/**
+ * @author Luuk Holleman
+ * @date 15 april
+ */
+package tspAlgorithm;
+
+public class Greedy implements TSPAlgorithm {
+
+	@Override
+	public String getName() {
+		return "Greedy";
+	}
+
+}
