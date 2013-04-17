1cb3ffc39868a86a76281ab296be265db6ae9a76
Luuk Holleman <luukholleman@gmail.com>
2013-04-17 13:47:29 +0200

    implemented xmluploadedlistener

--- src/XMLPanel.java
+++ src/XMLPanel.java
@@ -1,14 +1,19 @@
 import java.awt.Dimension;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import listener.XMLUploadedListener;
+
 
 public class XMLPanel extends JPanel {
 	private JButton xmlBtn = new JButton("XML Inlezen");
 	private JLabel fileLbl = new JLabel("Geen bestand gekozen");
+
+	private ArrayList<XMLUploadedListener> xmlUploadListeners = new ArrayList<XMLUploadedListener>();
 	
 	public XMLPanel()
 	{
@@ -17,6 +22,28 @@ public class XMLPanel extends JPanel {
 		setPreferredSize(new Dimension(500, 160));
 		
 		buildUI();
+	}
+	
+	/**
+	 * Voegt een listener toe, wordt getriggerd als er een nieuw bestand wordt geupload
+	 * 
+	 * @param xul
+	 */
+	public void addXMLUploadListener(XMLUploadedListener xul)
+	{
+		xmlUploadListeners.add(xul);
+	}
+	
+	/**
+	 * Triggert het xml uploaded event
+	 * 
+	 * @param xmlFileLocation
+	 */
+	private void xmlUploaded(String xmlFileLocation)
+	{
+		// trigger elk event
+		for(XMLUploadedListener xul : xmlUploadListeners)
+			xul.xmlUploaded(xmlFileLocation);
 	}
 	
 	private void buildUI()
---
+++ src/listener/XMLUploadedListener.java
@@ -0,0 +1,5 @@
+package listener;
+
+public interface XMLUploadedListener {
+	public abstract void xmlUploaded(String xmlFileLocation);
+}
