1c956ceefd7cf4ebfcb6bc83204e63aed35c1adf
Luuk Holleman <luukholleman@gmail.com>
2013-04-17 13:08:50 +0200

    organised imports

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 
-import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
