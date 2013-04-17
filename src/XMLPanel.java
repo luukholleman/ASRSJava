0e3a2b67c6052d4a4fc38ce18a52094e1fbf9f42
Luuk Holleman <luukholleman@gmail.com>
2013-04-17 14:46:24 +0200

    removed unused imports

--- src/XMLPanel.java
+++ src/XMLPanel.java
@@ -8,7 +8,6 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.filechooser.FileFilter;
 
