004e0ba3a313073ffd786cb7814660a2c6df48d5
Tim Potze <timpotze@gmail.com>
2013-04-23 12:44:08 +0200

    Added customer

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,25 @@
-
 public class Customer {
+	private int id;
+	private String name;
 
+	public Customer(int id, String name) {
+		this.setId(id);
+		this.setName(name);
+	}
+
+	private int getId() {
+		return id;
+	}
+
+	private void setId(int id) {
+		this.id = id;
+	}
+
+	private String getName() {
+		return name;
+	}
+
+	private void setName(String name) {
+		this.name = name;
+	}
 }
