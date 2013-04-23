37ba86a89dd28a15772d5c861e3399c63a288d90
Tim Potze <timpotze@gmail.com>
2013-04-23 12:45:00 +0200

    Removed setters

--- src/Customer.java
+++ src/Customer.java
@@ -11,15 +11,7 @@ public class Customer {
 		return id;
 	}
 
-	private void setId(int id) {
-		this.id = id;
-	}
-
 	private String getName() {
 		return name;
-	}
-
-	private void setName(String name) {
-		this.name = name;
 	}
 }
