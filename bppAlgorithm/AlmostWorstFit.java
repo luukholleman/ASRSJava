3ff453b36c4b300e9fdb242a00352f2eab16dd8d
Mike <mikevanloe@gmail.com>
2013-04-23 23:42:30 +0200

    Added to do list at feature-AWFbpp

--- bppAlgorithm/AlmostWorstFit.java
+++ bppAlgorithm/AlmostWorstFit.java
@@ -4,6 +4,8 @@
  */
 package bppAlgorithm;
 
+import asrs.Product;
+
 public class AlmostWorstFit implements BPPAlgorithm {
 	public static String name = "Almost Worst Fit";
 	
@@ -11,5 +13,12 @@ public class AlmostWorstFit implements BPPAlgorithm {
 	public String getName() {
 		return name;
 	}
-
+	
+	public void calculateBin(Product product){
+		/* Controleer welke bin het op 1 na volst zit
+		 * stuur het product die als input komt naar die bin
+		 */
+		
+	}
+	
 }
