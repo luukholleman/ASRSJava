51698c82105258125eb35f376d6b076d777d291f
Jorn000 <jorn.harkema@hotmail.com>
2013-04-24 10:44:45 +0200

    nothing

--- asrs/XMLLoader.java
+++ asrs/XMLLoader.java
@@ -1,8 +1,16 @@
 package asrs;
 
+import java.util.Date;
+
 public class XMLLoader {
 	
-	public Order readOrder(String filename){
-		return "hoi";
+	public Order readOrder(String path){
+		
+		File fXmlFile = new File("/Users/mkyong/staff.xml");
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(fXmlFile);
+		
+		Order(date, totalPrice, customer);
 	}
 }
--- src/XMLLoader.java
+++
@@ -1,8 +0,0 @@
-package src;
-
-public class XMLLoader {
-	
-	public Order readOrder(String filename){
-		return "hoi";
-	}
-}
