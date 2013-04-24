14a5fa4bea949aeacccbc797289c40add43213ae
Tim Potze <timpotze@gmail.com>
2013-04-24 10:03:51 +0200

    Added simple connection

--- asrs/DBHandler.java
+++ asrs/DBHandler.java
@@ -1,11 +1,38 @@
 package asrs;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Class om met de database te verbinden
+ * 
+ * @author timpotze
+ * 
+ */
 public class DBHandler {
 	// User details:
 	// 209.105.248.9
 	// asrs
 	// w1nd3sh31m
 
-	// TODO:
-	// Connect to database
+	public Connection connect() {
+		Connection conn = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+			String url = "jdbc:mysql://209.105.248.9/asrs";
+			return conn = DriverManager.getConnection(url, "asrs", "w1nd3sh31m");
+		} catch (InstantiationException e) {
+			//TODO: Handle error
+		} catch (IllegalAccessException e) {
+			//TODO: Handle error
+		} catch (ClassNotFoundException e) {
+			//TODO: Handle error
+		} catch (SQLException e) {
+			//TODO: Handle error
+		} finally {
+			return conn;
+		}
+	}
 }
