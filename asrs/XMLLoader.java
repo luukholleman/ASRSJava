524e749209fafdf9ef67b7883ec5e4b23812edd4
Jorn000 <jorn.harkema@hotmail.com>
2013-04-24 11:27:19 +0200

    nothing

--- asrs/XMLLoader.java
+++ asrs/XMLLoader.java
@@ -1,49 +1,9 @@
 package asrs;
 
-import java.util.Date;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
-import java.io.File;
-
 public class XMLLoader {
 	
 	public String readOrder(String path){
-		try {
-			File fXmlFile = new File(path);
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(fXmlFile);
-			
-			doc.getDocumentElement().normalize();
-			
-			System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-			 
-			NodeList nList = doc.getElementsByTagName("product");
-		 
-			System.out.println("----------------------------");
-		 
-			for (int temp = 0; temp < nList.getLength(); temp++) {
-		 
-				Node nNode = nList.item(temp);
-		 
-				System.out.println("\nCurrent Element :" + nNode.getNodeName());
-		 
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-		 
-					Element eElement = (Element) nNode;
-		 
-					System.out.println("Product number: " + eElement.getElementsByTagName("productnumber").item(0).getTextContent());
-		 
-				}
-			}
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-	    }
+		
 
 		
 		
