9237b39be4d79a155fa2c4780abc0d3d4f591ff9
Mike <mikevanloe@gmail.com>
2013-04-26 11:20:02 +0200

    Updated AlmostWorstFit algorithm

--- bppAlgorithm/AlmostWorstFit.java
+++ bppAlgorithm/AlmostWorstFit.java
@@ -5,12 +5,8 @@
 package bppAlgorithm;
 
 import java.util.ArrayList;
-<<<<<<< HEAD
 import java.util.Collections;
 import java.util.Comparator;
-=======
->>>>>>> 36ee10996a2cb7230dd6f051e7da795ae1a76e07
-
 import order.Product;
 import asrs.Bin;
 
@@ -24,12 +20,14 @@ public class AlmostWorstFit implements BPPAlgorithm {
 
 	@Override
 	public Bin calculateBin(Product product, ArrayList<Bin> bins) {
+		
 		Collections.sort(bins, new Comparator<Bin>() {
 			public int compare(Bin one, Bin two) {
 				return ((Integer) (one.getSize() - one.getFilled()))
 						.compareTo(two.getSize() - two.getFilled());
 			}
 		});
+
 		
 		return null;
 	}
