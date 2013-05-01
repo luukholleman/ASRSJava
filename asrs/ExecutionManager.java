d0591e6bacc3c4e541dc2b14506545f2648c79fd
Mike <mikevanloe@gmail.com>
2013-05-01 09:49:52 +0200

    Started working on the ExecutionManager

---
+++ asrs/ExecutionManager.java
@@ -0,0 +1,18 @@
+package asrs;
+
+import tspAlgorithm.TSPAlgorithm;
+import bppAlgorithm.BPPAlgorithm;
+
+public class ExecutionManager {
+	private Main main;
+	private Order order;
+	//private BinManager binManager;
+	//private Warehouse warehouse;
+	//private BinPacking binPacking;
+	private TSPAlgorithm tspAlgorithm;
+	private BPPAlgorithm bppAlgorithm;
+	private Integer width;
+	private Integer height;
+	private Boolean useDetectedSize;
+	
+}
