da2bcfac8ff0075c56b489c46b11a6266375c1fd
Mike <mikevanloe@gmail.com>
2013-05-01 10:12:48 +0200

    Added the baselines for the methods

--- asrs/ExecutionManager.java
+++ asrs/ExecutionManager.java
@@ -6,13 +6,31 @@ import bppAlgorithm.BPPAlgorithm;
 public class ExecutionManager {
 	private Main main;
 	private Order order;
-	//private BinManager binManager;
-	//private Warehouse warehouse;
-	//private BinPacking binPacking;
+	// private BinManager binManager;
+	// private Warehouse warehouse;
+	// private BinPacking binPacking;
 	private TSPAlgorithm tspAlgorithm;
 	private BPPAlgorithm bppAlgorithm;
 	private Integer width;
 	private Integer height;
 	private Boolean useDetectedSize;
+
+	public ExecutionManager(Main main, Order order, //BinManager binManager,
+			//Warehouse warehouse, BinPacking binPacking,
+			TSPAlgorithm tspAlgorithm, BPPAlgorithm bppAlgorithm,
+			Integer width, Integer height, Boolean useDetectedSize) {
+		
+	}
 	
+	public Byte detectedProduct(Byte red, Byte green, Byte blue){
+		return null;
+	}
+	
+	public void pickedUpProduct(Integer robotId){
+		
+	}
+	
+	public void deliveredProduct(Integer robotId){
+		
+	}
 }
