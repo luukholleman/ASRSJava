4619cd68126159887e78df24c88dc37ebc80c7ba
Mike <mikevanloe@gmail.com>
2013-05-03 11:10:40 +0200

    More ToDo comments

--- asrs/ExecutionManager.java
+++ asrs/ExecutionManager.java
@@ -6,39 +6,98 @@ import bppAlgorithm.BPPAlgorithm;
 public class ExecutionManager {
 	private Main main;
 	private Order order;
-//	private BinManager binManager;
-//	private Warehouse warehouse;
-//	private BinPacking binPacking;
+	private BinManager binManager;
+	private Warehouse warehouse;
+	private BinPacking binPacking;
 	private TSPAlgorithm tspAlgorithm;
 	private BPPAlgorithm bppAlgorithm;
 	private Integer width;
 	private Integer height;
 	private Boolean useDetectedSize;
 
-	public ExecutionManager(Main main, Order order, //BinManager binManager,
-//			Warehouse warehouse, BinPacking binPacking,
+	public ExecutionManager(Main main, Order order, BinManager binManager,
+			Warehouse warehouse, BinPacking binPacking,
 			TSPAlgorithm tspAlgorithm, BPPAlgorithm bppAlgorithm,
 			Integer width, Integer height, Boolean useDetectedSize) {
-
+		if(warehouse.getRobots() == 2){
+			/**
+			 * splits het magazijn
+			 * gebruik het algoritme op beide delen.
+			 * commandeer bot/sim
+			 */
+		} else {
+			/**
+			 * Pas het algoritme toe op het magazijn
+			 * commandeer bot/sim
+			 */
+		}
+		
 	}
 	
 	public Byte detectedProduct(Byte red, Byte green, Byte blue){
 		/**
 		 * Oproepen door bppArduino, kleur naar grootte omzetten.
-		 * byte wordt binnummer?
+		 * byte wordt binnummer-1 (0 is bin 1, 1 is bin 2, etc.)
 		 */
 		return null;
 	}
 	
 	public void pickedUpProduct(Integer robotId){
 		/**
-		 * Geeft volgende locatie in een byte[]
+		 * Geeft volgende locatie, tenzij er geen locatie meer is, dan naar binpacker gaan
 		 */
 	}
 	
 	public void deliveredProduct(Integer robotId){
 		/**
-		 * iets over het product afleveren
+		 * producten afgeleverd, ga terug naar begin
 		 */
 	}
+	
+	
+	
+	/**
+	 * Alle getters
+	 */
+	public Main getMain() {
+		return main;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public BinManager getBinManager() {
+		return binManager;
+	}
+
+	public Warehouse getWarehouse() {
+		return warehouse;
+	}
+
+	public BinPacking getBinPacking() {
+		return binPacking;
+	}
+
+	public TSPAlgorithm getTspAlgorithm() {
+		return tspAlgorithm;
+	}
+
+	public BPPAlgorithm getBppAlgorithm() {
+		return bppAlgorithm;
+	}
+
+	public Integer getWidth() {
+		return width;
+	}
+
+	public Integer getHeight() {
+		return height;
+	}
+
+	public Boolean getUseDetectedSize() {
+		return useDetectedSize;
+	}
+	
+	
 }
