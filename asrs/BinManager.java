01838b67f30d86a6ec361f7c2ca2f569725d843c
Mike <mikevanloe@gmail.com>
2013-05-03 15:35:39 +0200

    added methods to BinManager

--- asrs/BinManager.java
+++ asrs/BinManager.java
@@ -1,5 +1,15 @@
 package asrs;
 
+import java.util.ArrayList;
+
 public class BinManager {
+	private ArrayList<Bin> bins;
 	
+	public void addBin(Bin bin){
+		this.bins.add(bin);
+	};
+	
+	public void clearBins(){
+		this.bins.clear();
+	};
 }
