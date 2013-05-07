0e6135b5f3301ec810ba5200412facb5d1b4a388
Tim Potze <timpotze@gmail.com>
2013-05-07 13:00:23 +0200

    Exceptions, everyone loves them.

---
+++ asrs/ProductNotFoundException.java
@@ -0,0 +1,9 @@
+package asrs;
+
+public class ProductNotFoundException extends Exception {
+
+	public ProductNotFoundException(String message)
+	{
+		super(message);
+	}
+}
--- tspAlgorithm/TSPAlgorithm.java
+++ tspAlgorithm/TSPAlgorithm.java
@@ -12,5 +12,10 @@ import order.Product;
 public interface TSPAlgorithm {
 	public abstract String getName();
 	
+	/**
+	 * Calculates best route for the products that should be on-route
+	 * @param products the products that have to be picked up
+	 * @return list of products in the proper order
+	 */
 	public abstract ArrayList<Product> calculateRoute(ArrayList<Product> products);
 }
