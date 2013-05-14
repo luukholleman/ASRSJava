d7810757715522686cdbacb445769c1481a3cfb5
Jorn000 <jorn.harkema@hotmail.com>
2013-05-14 10:50:25 +0200

    added (empty) methods

--- asrsController/Arduino.java
+++ asrsController/Arduino.java
@@ -1,5 +1,21 @@
 package asrsController;
 
 public class Arduino {
-
+	
+	public Arduino (String port){
+		
+	}
+	
+	public void sendBytes(Byte[] bytes){
+		
+	}
+	
+	public void sendByte(Byte b){
+		
+	}
+	
+	public void serialEvent(SerialPortEvent oEvent){
+		
+	}
+	
 }
