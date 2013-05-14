a560b08a57e8f2f2c7dbf9f778253122f3994129
Jorn000 <jorn.harkema@hotmail.com>
2013-05-14 11:32:02 +0200

    build serialEvent

--- asrsController/Arduino.java
+++ asrsController/Arduino.java
@@ -1,6 +1,15 @@
 package asrsController;
 
-public class Arduino {
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+ 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
+
+public class Arduino implements SerialPortEventListener{
 	
 	public Arduino (String port){
 		
@@ -15,7 +24,18 @@ public class Arduino {
 	}
 	
 	public void serialEvent(SerialPortEvent oEvent){
-		
+		if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+			try {
+	        	int myByte=input.read();
+	            int value = myByte & 0xff; //byte to int conversion:0...127,-127...0 -> 0...255
+	            if(value>=0 && value<256){ //make sure everything is OK
+	            	System.out.println(value);
+	                sendByte((byte)myByte);
+	            }
+	        } catch (Exception e) {
+	        	System.err.println(e.toString());
+	        }
+		}
 	}
 	
 }
