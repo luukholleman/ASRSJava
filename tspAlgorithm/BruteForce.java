e745d75b24645a19c7a84e666020027545400030
Tim Potze <timpotze@gmail.com>
2013-05-19 12:58:43 +0200

    BruteForce optimalized (Hope it works...)

--- tspAlgorithm/BruteForce.java
+++ tspAlgorithm/BruteForce.java
@@ -15,8 +15,18 @@ import order.Product;
  * 
  */
 public class BruteForce extends TSPAlgorithm {
-	public static String name = "Brute Force";
+	
+	private static final float INFINITY = 99999.0f;
+	
+	// De berekende snelste route tot een betere is gevonden
+	private ArrayList<Product> fastest = null;
+	private float length = INFINITY;
 
+	
+	
+	
+	public static String name = "Brute Force";
+	
 	@Override
 	public String getName() {
 		return BruteForce.name;
@@ -26,10 +36,6 @@ public class BruteForce extends TSPAlgorithm {
 	public ArrayList<Product> calculateRoute(ArrayList<Product> products,
 			int numberOfRobots, int currentRobot) {
 		products = splitOrder(products, numberOfRobots, currentRobot);
-
-		// De berekende snelste route tot een betere is gevonden
-		ArrayList<Product> fastest = null;
-		float length = 0.0f;
 
 		// Als de lijst leeg is, geef deze terug
 		if (products.size() == 0)
@@ -44,21 +50,7 @@ public class BruteForce extends TSPAlgorithm {
 		tmp.add(products.get(0));
 
 		// Ga door alle routes in de permutaties heen
-		for (ArrayList<Product> route : permute(products, tmp)) {
-			// bereken de totale lengte van de route
-			float currentLength = 0;
-			for (int i = 1; i < route.size(); i++) {
-				currentLength += route.get(i).getLocation()
-						.getDistanceTo(route.get(i - 1).getLocation());
-			}
-
-			// Als er nog geen snelste route is of deze route korter is dan de
-			// vorige korste route, sla deze route op
-			if (fastest == null || currentLength < length) {
-				fastest = route;
-				length = currentLength;
-			}
-		}
+		permute(products, tmp);
 
 		return fastest;
 	}
@@ -72,13 +64,8 @@ public class BruteForce extends TSPAlgorithm {
 	 * @param current
 	 *            de huidige lijst van product dat berekend is, maar nog geen
 	 *            compleet pad is(niet zo lang als origin)
-	 * @return alle mogelijke product volgordes berekend
 	 */
-	private ArrayList<ArrayList<Product>> permute(ArrayList<Product> origin,
-			ArrayList<Product> current) {
-
-		// Lijst om alle mogelijke volgordes op te slaan
-		ArrayList<ArrayList<Product>> dest = new ArrayList<ArrayList<Product>>();
+	private void permute(ArrayList<Product> origin, ArrayList<Product> current) {
 
 		// Voeg een product toe op elke mogelijke locatie in de lijst
 		for (int i = 0; i < current.size() + 1; i++) {
@@ -91,12 +78,21 @@ public class BruteForce extends TSPAlgorithm {
 
 			// Als de nieuwe lijst alle producten bevat, geef deze terug,
 			// anders, herhaal deze functie
-			if (tmp.size() == origin.size())
-				dest.add(tmp);
+			if (tmp.size() == origin.size()) {
+				float currentLength = 0;
+				for (int j = 1; j < tmp.size(); j++)
+					currentLength += tmp.get(j).getLocation().getDistanceTo(tmp.get(j - 1).getLocation());
+
+				// Als er nog geen snelste route is of deze route korter is dan de
+				// vorige korste route, sla deze route op
+				if (currentLength < length) {
+					fastest = tmp;
+					length = currentLength;
+				}
+			}
 			else
-				dest.addAll(permute(origin, tmp));
+				permute(origin, tmp);
 		}
-		return dest;
 	}
 
 }
