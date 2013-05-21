f8152e8d6d024ad5c236180edf95b981abb7a01e
Mike <mikevanloe@gmail.com>
2013-05-21 14:49:57 +0200

    modified BinPackingArduino

--- asrsController/BinPackingArduino.java
+++ asrsController/BinPackingArduino.java
@@ -7,8 +7,14 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class BinPackingArduino extends Arduino implements BinPacking{
+	private ExecutionManager executionManager;
+	private int bins;
+	private CommPortIdentifier port;
 	
-	public BinPackingArduino (CommPortIdentifier port){
+	public BinPackingArduino (ExecutionManager executionManager, int bins, CommPortIdentifier port){
 		super(port);
+		this.executionManager = executionManager;
+		this.bins = bins;
 	}
+	
 }
