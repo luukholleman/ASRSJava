f720c3285fdacb34b5ba79d28d12a8326cc7a7bf
Jorn000 <jorn.harkema@hotmail.com>
2013-05-29 13:58:38 +0200

    fixed XMLLoader after codereview

--- asrs/XMLLoader.java
+++ asrs/XMLLoader.java
@@ -19,30 +19,32 @@ import java.util.*;
  */
 public class XMLLoader {
 	
+	/**
+	 * Functie leest de data uit het XML bestand en zet deze om in een order object
+	 * 
+	 * @param path Pad naar het bestand
+	 * @return order
+	 */
 	public static Order readOrder(String path) throws ProductNotFoundException{
 		
 		// Maakt een order aan en maakt hem leeg, deze wordt later gevuld met informatie
 		Order order = null;
 		
 		try {
-			// Bouwt een JDOM document van het XML bestand met behulp van een SAX parser
-			SAXBuilder builder = new SAXBuilder();
-			File xmlFile = new File(path);
-			Document document = (Document) builder.build(xmlFile);
+			// Bouwt het document
+			Document document = buildDocument(path);
 			
+			// Begin node aanwijzen
 			Element rootNode = document.getRootElement();
 			
 			// Haalt de datum uit het XML bestand
-			String dateXML = rootNode.getChildText("date");
-			Date date = new SimpleDateFormat("dd-MM-yyyy").parse(dateXML);
+			Date date = getDate(rootNode);
 	
 			// Haalt de totale prijs uit het XML bestand
 			float totalPrice = Float.parseFloat(rootNode.getChildText("totalprice"));
 	   
 			// Haalt de klant informatie uit het XML bestand
-			int customerId = Integer.parseInt(rootNode.getChildText("customernumber"));
-			String customername = rootNode.getChildText("customername");
-			Customer customer = new Customer(customerId, customername);
+			Customer customer = getCustomerInfo(rootNode);
 
 			//Vul de order met informatie
 			order = new Order(date, totalPrice, customer);
@@ -50,27 +52,18 @@ public class XMLLoader {
 			// Haalt de producten uit het XML bestand doormiddel van een for-loop
 			List<Element> list = rootNode.getChildren("product");
 			for (int i = 0; i < list.size(); i++) {
-				
-				Element node = (Element) list.get(i);
-				int productId = Integer.parseInt(node.getChildText("productnumber"));
-				String description = node.getChildText("description");
-				float price = Float.parseFloat(node.getChildText("price"));
-				
-				//Haal het aangegeven aantal producten op
-				for(int n = 0 ; n < Integer.parseInt(node.getChildText("amount")); n++) {
-					Product product = new Product(productId, description, price);
-					order.addProduct(product);
-				}
+				// Haalt een product uit het xml bestand
+				getProductFromXML(order, list, i);
 			}
 		}
 		
 		// Vangt de eventuele foutmeldingen op
 		catch (IOException io) {
-			System.out.println(io.getMessage());
+			io.printStackTrace();
 		}
 		
 		catch (JDOMException jdomex) {
-			System.out.println(jdomex.getMessage());
+			jdomex.printStackTrace();
 		} 
 		
 		catch (ParseException e) {
@@ -79,5 +72,65 @@ public class XMLLoader {
 		
 		// Returnt de gevulde order
 		return order;
+	}
+	
+	/**
+	 * Haalt een product uit het XML bestand en zet hem in de order
+	 * 
+	 * @param order 
+	 * @param list Lijst met producten
+	 * @param i Teller van de for loop
+	 */
+	private static void getProductFromXML(Order order, List<Element> list, int i)
+			throws ProductNotFoundException {
+		Element node = (Element) list.get(i);
+		int productId = Integer.parseInt(node.getChildText("productnumber"));
+		String description = node.getChildText("description");
+		float price = Float.parseFloat(node.getChildText("price"));
+		
+		//Haal het aangegeven aantal producten op
+		for(int n = 0 ; n < Integer.parseInt(node.getChildText("amount")); n++) {
+			Product product = new Product(productId, description, price);
+			order.addProduct(product);
+		}
+	}
+
+	/**
+	 * Haalt de klant informatie uit het XML bestand
+	 * 
+	 * @param rootNode De standaard node
+	 * @return customer
+	 */
+	private static Customer getCustomerInfo(Element rootNode) {
+		int customerId = Integer.parseInt(rootNode.getChildText("customernumber"));
+		String customername = rootNode.getChildText("customername");
+		Customer customer = new Customer(customerId, customername);
+		return customer;
+	}
+
+	/**
+	 * Haalt de datum uit het XML bestand
+	 * 
+	 * @param rootNode De standaard node
+	 * @return date
+	 */
+	private static Date getDate(Element rootNode) throws ParseException {
+		String dateXML = rootNode.getChildText("date");
+		Date date = new SimpleDateFormat("dd-MM-yyyy").parse(dateXML);
+		return date;
+	}
+	
+	/**
+	 * Bouwt een JDOM document van het XML bestand met behulp van een SAX parser
+	 * 
+	 * @param path Pad naar het bestand
+	 * @return document
+	 */
+	private static Document buildDocument(String path) throws JDOMException,
+			IOException {
+		SAXBuilder builder = new SAXBuilder();
+		File xmlFile = new File(path);
+		Document document = (Document) builder.build(xmlFile);
+		return document;
 	}
 }
