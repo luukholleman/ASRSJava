8e8b8e5a0ae19e734e0c804ba383a9df374f46b5
Mike <mikevanloe@gmail.com>
2013-05-29 16:06:37 +0200

    Added JDoc to AWF, BF, FF and WF algorithms

--- bppAlgorithm/AlmostWorstFit.java
+++ bppAlgorithm/AlmostWorstFit.java
@@ -1,7 +1,3 @@
-/**
- * @author Luuk Holleman
- * @date 16 april
- */
 package bppAlgorithm;
 
 import java.util.ArrayList;
@@ -9,17 +5,18 @@ import java.util.Collections;
 import java.util.Comparator;
 import order.Product;
 
+/**
+ * Het Almost Worst Fit algoritme berekent welke bin de op een na meeste ruimte
+ * heeft en of het product daar in past
+ * 
+ * @author Mike
+ * 
+ */
 public class AlmostWorstFit implements BPPAlgorithm {
-	public static String name = "Almost Worst Fit";
-
-	@Override
 	/**
-	 * Geeft de naam van het algoritme aan de GUI
-	 * @return name
+	 * Naam
 	 */
-	public String getName() {
-		return name;
-	}
+	public static final String NAME = "Almost Worst Fit";
 
 	@Override
 	/**
@@ -30,38 +27,33 @@ public class AlmostWorstFit implements BPPAlgorithm {
 	 * @return fittingBin
 	 */
 	public Bin calculateBin(Product product, ArrayList<Bin> bins) {
-		//Kopieer de arraylist
-		bins = new ArrayList<Bin>(bins);		
-		
-		//Sorteer de bins in op volgorde van veel inhoud naar weinig inhoud
+		// Kopieer de arraylist
+		bins = new ArrayList<Bin>(bins);
+
+		// Sorteer de bins in op volgorde van veel inhoud naar weinig inhoud
 		Collections.sort(bins, new Comparator<Bin>() {
 			public int compare(Bin one, Bin two) {
 				return ((Integer) (one.getSize() - one.getFilled()))
 						.compareTo(two.getSize() - two.getFilled());
 			}
 		});
-		//Controleer of het product past
+		// Controleer of het product past
 		ArrayList<Bin> possibleBins = new ArrayList<Bin>();
-		for(Bin bin : bins){
-			if ((bin.getSize()-bin.getFilled()) >= product.getSize())
+		for (Bin bin : bins) {
+			if ((bin.getSize() - bin.getFilled()) >= product.getSize())
 				possibleBins.add(bin);
 		}
-		//Verwijder de bin met de meeste ruimte
-		if(possibleBins.size() > 1)
-			possibleBins.remove(possibleBins.get(possibleBins.size()-1));
-		//Return de bin die daarna de meeste ruimte heeft, of als alles vol zit de overflowbin
+		// Verwijder de bin met de meeste ruimte
+		if (possibleBins.size() > 1)
+			possibleBins.remove(possibleBins.get(possibleBins.size() - 1));
+		// Return de bin die daarna de meeste ruimte heeft, of als alles vol zit
+		// de overflowbin
 		Bin fittingBin;
-		if(possibleBins.isEmpty())
+		if (possibleBins.isEmpty())
 			fittingBin = null;
 		else
-			fittingBin = possibleBins.get(possibleBins.size()-1);
-		
-		return fittingBin; 
-	}
+			fittingBin = possibleBins.get(possibleBins.size() - 1);
 
-	@Override
-	public int getBinCount() {
-		return 0;
+		return fittingBin;
 	}
-
 }
--- bppAlgorithm/WorstFit.java
+++ bppAlgorithm/WorstFit.java
@@ -1,27 +1,23 @@
-/**
- * @author Luuk Holleman
- * @date 16 april
- */
 package bppAlgorithm;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-
 import order.Product;
 
+/**
+ * Het Worst Fit algoritme berekent welke bin er de meeste ruimte heeft en of
+ * het product daar in past
+ * 
+ * @author Mike
+ * 
+ */
 public class WorstFit implements BPPAlgorithm {
-	public static String name = "Worst Fit";
-
-	@Override
 	/**
-	 * Geeft de naam van het algoritme aan de GUI
-	 * @return name
+	 * Naam
 	 */
-	public String getName() {
-		return name;
-	}
+	public static final String NAME = "Worst Fit";
 
 	@Override
 	/**
@@ -32,35 +28,29 @@ public class WorstFit implements BPPAlgorithm {
 	 * @return fittingBin
 	 */
 	public Bin calculateBin(Product product, ArrayList<Bin> bins) {
-		//Kopieer de arraylist
-		bins = new ArrayList<Bin>(bins);		
+		// Kopieer de arraylist
+		bins = new ArrayList<Bin>(bins);
 
-		//Sorteer de bins in op volgorde van veel inhoud naar weinig inhoud
+		// Sorteer de bins in op volgorde van veel inhoud naar weinig inhoud
 		Collections.sort(bins, new Comparator<Bin>() {
 			public int compare(Bin one, Bin two) {
 				return ((Integer) (one.getSize() - one.getFilled()))
 						.compareTo(two.getSize() - two.getFilled());
 			}
 		});
-		//Controleer of het product past
+		// Controleer of het product past
 		ArrayList<Bin> possibleBins = new ArrayList<Bin>();
-		for(Bin bin : bins){
-			if ((bin.getSize()-bin.getFilled()) >= product.getSize())
+		for (Bin bin : bins) {
+			if ((bin.getSize() - bin.getFilled()) >= product.getSize())
 				possibleBins.add(bin);
 		}
-		//Return de bin die de meeste ruimte heeft
+		// Return de bin die de meeste ruimte heeft
 		Bin fittingBin;
-		if(possibleBins.isEmpty())
+		if (possibleBins.isEmpty())
 			fittingBin = null;
 		else
-			fittingBin = possibleBins.get(possibleBins.size()-1);
-		
-		return fittingBin;
-	}
+			fittingBin = possibleBins.get(possibleBins.size() - 1);
 
-	@Override
-	public int getBinCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return fittingBin;
 	}
 }
